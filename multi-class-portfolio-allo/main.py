5ed71820ee3573d48a03be1743aa83d7eadd1814
jeonghyun <jeonghn.park@gmail.com>
2021-09-16 17:44:37 +0900

    multi-calss portfolio2

--- multi-class-portfolio-allo/main.py
+++ multi-class-portfolio-allo/main.py
@@ -7,15 +7,20 @@ pd.set_option('display.max_columns', 50)
 df = pd.read_csv('Case3HistoricalPrices.csv', index_col=0)  # set the first col is index
 print(df.head())
 
-df.columns[0]
-df['S1'].shift(1)
+# df.columns[0]
+# df['S1'].shift(1)
+#
+# df[df.columns[0:2]]
+# df[['S1','S2']]
+#
+# df[df.columns[0]]==df['S1'] #True
+# df.columns[0]==df['S1'] #False
 
-df[df.columns[0:2]]
-df[['S1','S2']]
 
-df[df.columns[0]]==df['S1'] #True
-df.columns[0]==df['S1'] #False
+# df['shift']=df['S1'].shift(1)
 
+def make_return_series(df):
+    df['PctChg']=(df[df.columns[0]]-df[df.columns[0]].shift(1))/df[df.columns[0]].shift(1)
+    return df
 
-df['shift']=df['S1'].shift(1)
-df['PctChg']=df[df.columns[0]]-df[df.columns[0]].shift(1))
+df=make_return_series(df)
